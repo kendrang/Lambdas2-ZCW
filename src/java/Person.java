8d305e54afa4dd5b28c1a825ea19ea53733e19fb
kendrang <kendrang@users.noreply.github.com>
2019-11-29 13:50:36 -0500

    made the classes

---
+++ src/java/Person.java
@@ -0,0 +1,57 @@
+import java.lang.reflect.Array;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class Person {
+
+    public enum Sex {
+        MALE, FEMALE
+    }
+
+    String name;
+    LocalDate birthday;
+    Sex gender;
+    String emailAddress;
+    ArrayList<Person> peopleList = new ArrayList<>();
+
+    public Person(String name, LocalDate birthday, Sex gender, String emailAddress) {
+        this.name = name;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.emailAddress = emailAddress;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public Sex getGender() {
+        return gender;
+    }
+
+    public void setGender(Sex gender) {
+        this.gender = gender;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+
+
+}
---
+++ src/java/checkPerson.java
@@ -0,0 +1,5 @@
+public interface checkPerson {
+
+    Boolean test (Person person);
+
+}
